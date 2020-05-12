fe42930cb0e813f83c803bc74e236492c0d258d4
Kendy205 <56836089+Kendy205@users.noreply.github.com>
2020-05-12 20:57:12 +0200

    Upload dosavadních souborů
    Uploaudnutí dosavadních front-end souborů, index.html, a základy search.html společně s backgroundy pro search.html a jejich css styli

---
+++ main.js
@@ -0,0 +1,6 @@
+// To co jsem stealnul
+
+import {MDCTextField} from '@material/ripple';
+import {MDCTextField} from '@material/textfield';
+
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
