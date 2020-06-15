2a181a7c096d0adfc1cbfbc0d49cfbc56231a821
Kendy205 <56836089+Kendy205@users.noreply.github.com>
2020-06-15 11:57:31 +0200

    Nová navigace a Hesla

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+//!Heslo
+
+function passConfirm() {
+  var pass = document.getElementById('pass').value;
+  console.log(pass);
+  document.getElementById('confirm').href = pass;
+}
+
 // To co jsem stealnul
 
-import {MDCTextField} from '@material/ripple';
-import {MDCTextField} from '@material/textfield';
+import { MDCTextField } from '@material/ripple';
+import { MDCTextField } from '@material/textfield';
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
